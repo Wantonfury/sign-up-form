8a27fd1aa81ef0ea72b60a8f00bc7f6249d83883
Wantonfury <orzan.alexandru94@gmail.com>
2022-11-15 13:10:10 +0200

    Add error message when password not confirmed

--- js/pwd_confirm.js
+++ js/pwd_confirm.js
@@ -2,15 +2,22 @@ let btn = document.getElementById("btn-create");
 let form = document.getElementById("form");
 let pwd = document.getElementById("pwd");
 let pwd_confirm = document.getElementById("pwd-confirm");
+let pwd_error = document.getElementById("pwd-error");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    if (checkPassword())
+    if (checkPassword()) {
+        pwd.classList.remove("error");
+        pwd_confirm.classList.remove("error");
+        pwd_error.classList.add("hidden");
+        
         form.submit();
+    }
     else {
         pwd.classList.add("error");
         pwd_confirm.classList.add("error");
+        pwd_error.classList.remove("hidden");
     }
 });
 
